2f50d94499fe5c04f72d153e499dda8dd50b5235
Nasko Vasilev <nasko.vasilev@betvictor.com>
2016-02-11 16:50:13 +0100

    results

--- src/hashcode2016/App.java
+++ src/hashcode2016/App.java
@@ -8,9 +8,7 @@ import hashcode2016.processor.SquareProcessor;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @author vasilevn
@@ -22,58 +20,63 @@ public class App {
 
     public static void main(String[] args) {
         App app = new App();
-        String fileName = "test/learn_and_teach.in";
+        CommandPrinter commandPrinter = new CommandPrinter();
 
-        try (Scanner scanner = new Scanner(Paths.get(fileName))) {
-            String line;
+        String[] fileNames = {"learn_and_teach", "logo", "right_angle"};
 
-            int row = -1;
-            while (scanner.hasNext()){
-                line = scanner.nextLine();
+        for (String fileName: fileNames) {
+            try (Scanner scanner = new Scanner(Paths.get("test/" + fileName + ".in"))) {
+                String line;
 
-                if (row == -1) {
-                    String[] sizeNums = line.trim().split(" ");
-                    int rowNum = Integer.parseInt(sizeNums[0]);
-                    int colNum = Integer.parseInt(sizeNums[1]);
-                    app.board = new Board(rowNum, colNum);
-                } else {
-                    for (int col = 0; col < app.board.colNum; col++) {
-                        app.board.cells[row][col] = line.charAt(col);
+                int row = -1;
+                while (scanner.hasNext()){
+                    line = scanner.nextLine();
+
+                    if (row == -1) {
+                        String[] sizeNums = line.trim().split(" ");
+                        int rowNum = Integer.parseInt(sizeNums[0]);
+                        int colNum = Integer.parseInt(sizeNums[1]);
+                        app.board = new Board(rowNum, colNum);
+                    } else {
+                        for (int col = 0; col < app.board.colNum; col++) {
+                            app.board.cells[row][col] = line.charAt(col);
+                        }
                     }
+
+                    row++;
                 }
 
-                row++;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            List<Command> allCommands = new ArrayList<>();
 
-        List<Command> allCommands = new ArrayList<>();
+            app.board.prettyPrint();
 
-        app.board.prettyPrint();
+            // Processing / Computation
+            SquareProcessor squareProcessor = new SquareProcessor(app.board);
+            squareProcessor.process();
+            allCommands.addAll(squareProcessor.squareCommands);
+            allCommands.addAll(squareProcessor.eraseCellCommands);
+            app.board.prettyPrint();
 
-        // Processing / Computation
-        SquareProcessor squareProcessor = new SquareProcessor(app.board);
-        squareProcessor.process();
-        allCommands.addAll(squareProcessor.squareCommands);
-        allCommands.addAll(squareProcessor.eraseCellCommands);
-        app.board.prettyPrint();
+            LineProcessor lineProcessor = new LineProcessor(app.board);
+            lineProcessor.process();
+            allCommands.addAll(lineProcessor.lineCommands);
+            app.board.prettyPrint();
 
-        LineProcessor lineProcessor = new LineProcessor(app.board);
-        lineProcessor.process();
-        allCommands.addAll(lineProcessor.lineCommands);
-        app.board.prettyPrint();
+            SingleCellProcessor singleCellProcessor = new SingleCellProcessor(app.board);
+            singleCellProcessor.process();
+            allCommands.addAll(singleCellProcessor.cellCommands);
+            app.board.prettyPrint();
 
-        SingleCellProcessor singleCellProcessor = new SingleCellProcessor(app.board);
-        singleCellProcessor.process();
-        allCommands.addAll(singleCellProcessor.cellCommands);
-        app.board.prettyPrint();
-
-        System.out.println(allCommands.size());
+            System.out.println(allCommands.size());
 //        allCommands.stream().forEach((Command command) -> {
 //            System.out.println(command);
 //        });
-    }
 
+           commandPrinter.print(allCommands, "test/" + fileName + ".out");
+        }
+    }
 }
